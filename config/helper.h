c9c1b75b3bdbf5faff8bf19d8e0297d1d8fd36dc
John Entwistle <j.entwistle76@gmail.com>
2025-12-30 12:51:20 -0500

    Refactor keymap using layer helper macros

--- config/helper.h
+++ config/helper.h
@@ -10,8 +10,8 @@
 #define ZMK_HELPER_STRINGIFY(x) #x
 
 // Preprocessor mechanism to overload macros, cf. https://stackoverflow.com/a/27051616/6114651
-#define VARGS_(_10, _9, _8, _7, _6, _5, _4, _3, _2, _1, N, ...) N
-#define VARGS(...) VARGS_(__VA_ARGS__, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+#define VARGS_(_20, _19, _18, _17, _16, _15, _14, _13, _12, _11, _10, _9, _8, _7, _6, _5, _4, _3, _2, _1, N, ...) N
+#define VARGS(...) VARGS_(__VA_ARGS__, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
 #define CONCAT_(a, b) a##b
 #define CONCAT(a, b) CONCAT_(a, b)
 
