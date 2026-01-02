65d9db75f02694f7f6db4fb7bb1beb9f0fe3ba40
John Entwistle <j.entwistle76@gmail.com>
2026-01-01 22:20:36 -0500

    WIP add keymap drawer

--- build.west.sh
+++ build.west.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-IMAGE=glove80-zmk-docker-west
+IMAGE=glove80-zmk-config-west
 CONFDIR="${PWD}/config"
+DRAWDIR="${PWD}/draw"
 
 docker build -t "$IMAGE" -f Dockerfile.west .
-docker run -it --rm -v "$CONFDIR:/config" -e UID="$(id -u)" -e GID="$(id -g)" "$IMAGE" /bin/bash
+docker run -it --rm -v "$CONFDIR:/config" -v "$DRAWDIR:/draw"  -e UID="$(id -u)" -e GID="$(id -g)" "$IMAGE" /bin/bash
